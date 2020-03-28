bda70420e2e061eed258a6e95d9fe5e91f064507
xcalad01 <caladifilip@gmail.com>
2020-03-28 16:55:59 +0100

    fix lastDate

--- resources/js/components/Component.vue
+++ resources/js/components/Component.vue
@@ -73,7 +73,8 @@ import ApexCharts from "apexcharts";
     var real_time_data = [];
 
     function getNewSeries(lastDate, min_max){
-        var result = {x: _.cloneDeep(lastDate) + 86400000, y: Math.floor(Math.random() * (min_max.max - min_max.min + 1)) + min_max.min};
+        var date = _.cloneDeep(lastDate);
+        var result = {x: date + 86400000, y: Math.floor(Math.random() * (min_max.max - min_max.min + 1)) + min_max.min};
         lastDate += 86400000;
         return result;
     }
