8739cb5a9734fc01c0bd8d67030f820bb1da1598
Filip Caladi <filip.caladi@kiwi.com>
2020-04-09 19:34:04 +0200

    Bitfinex init_db added

--- app/Modules/BitFinex.php
+++ app/Modules/BitFinex.php
@@ -28,6 +28,9 @@ class BitFinex extends Base
 
     private $timestamp = null;
 
+    private $init_db_start_timestamp = "1325376000000";
+    private $init_db_stop_timestamp = "1585699200000";
+
     private function send_get(){
         $results = array();
         foreach ($this->config as $item){
@@ -76,6 +79,60 @@ class BitFinex extends Base
         $payload = $this->send_get();
         if (!empty($payload)){
             $this->send_post($payload);
+        }
+    }
+
+    public function run_init_db_task(){
+        foreach ($this->config as $config_item) {
+            $from = strtolower(substr($config_item,1, 3));
+            $to = strtolower(substr($config_item, 4, 3));
+
+            $url = "https://api-pub.bitfinex.com/v2/candles/trade:1m:t{$config_item}/hist?limit=10000&start={$this->init_db_start_timestamp}&sort=1";
+            $this->set_curl_url($url);
+            $data = $this->do_send_get();
+
+            $last_timestamp = null;
+
+            $do_break = false;
+
+            while (true) {
+                $results = array();
+                foreach ($data as $item) {
+                    if ($item[0] == $this->init_db_stop_timestamp) {
+                        $do_break = true;
+                        break;
+                    }
+                    array_push($results, array(
+                        "Exchange_id" => $this->exchange_id,
+                        "From" => $from,
+                        "To" => $to,
+                        "Timestamp" => $item[0] / 1000,
+                        "Historical" => array(
+                            null,
+                            $item[1],
+                            $item[3],
+                            $item[4],
+                            $item[2],
+                            $item[5]
+                        )
+                    ));
+
+                    $last_timestamp = $item[0];
+                }
+
+                if ($do_break) {
+                    break;
+                }
+
+                if (!empty($results)){
+                    $this->send_post($results);
+                }
+
+                print_r("Querying from new timestamp\n");
+                $url = "https://api-pub.bitfinex.com/v2/candles/trade:1m:tBTCUSD/hist?limit=10000&start={$last_timestamp}&sort=1";
+                $this->set_curl_url($url);
+                $data = $this->do_send_get();
+            }
         }
     }
 
