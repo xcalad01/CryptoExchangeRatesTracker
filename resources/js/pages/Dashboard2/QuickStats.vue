66b1acbd1e60770c0ef8bbd8c560f0047f3eedee
Filip Caladi <filip.caladi@kiwi.com>
2020-05-02 17:03:46 +0200

    update

--- resources/js/pages/Dashboard2/QuickStats.vue
+++ resources/js/pages/Dashboard2/QuickStats.vue
@@ -92,8 +92,8 @@
                     return item[1];
                 });
 
-                this.h24_min = Math.min(values).toFixed(3);
-                this.h24_max = Math.max(values).toFixed(3);
+                this.h24_min = Math.min(...values).toFixed(3);
+                this.h24_max = Math.max(...values).toFixed(3);
             }
         },
 
