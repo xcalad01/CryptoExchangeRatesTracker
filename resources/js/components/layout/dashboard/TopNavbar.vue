2613fbb0db9a458674208a3dc2b9d8cee1089565
Filip Caladi <filip.caladi@kiwi.com>
2020-05-06 18:59:07 +0200

    add links

--- resources/js/components/layout/dashboard/TopNavbar.vue
+++ resources/js/components/layout/dashboard/TopNavbar.vue
@@ -1,10 +1,10 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-absolute" style="justify-content: center"
        :class="{'bg-white': showMenu, 'navbar-transparent': !showMenu, 'navbar-wrapper': true}">
-    <a style="font-size: 250%; padding-right: 2%">{{routeName}}</a>
+    <a v-bind:href="this.exchange_links[this.$route.meta['title']]" style="font-size: 250%; padding-right: 2%; color: #f1f1e6">{{routeName}}</a>
 
-    <cryptoicon v-if="isasset" v-bind:symbol="asset" size="50"/>
-    <i v-else v-bind:class="icon_class"></i>
+    <a v-if="isasset" v-bind:href="this.crypto_links[this.$route.name]"><cryptoicon v-bind:symbol="asset" size="50"/></a>
+      <a v-else v-bind:href="this.exchange_links[this.$route.meta['title']]"><i v-bind:class="icon_class"></i></a>
 
   </nav>
 </template>
@@ -42,7 +42,46 @@
         activeNotifications: false,
         showMenu: false,
         searchModalVisible: false,
-        searchQuery: ''
+        searchQuery: '',
+          exchange_links: {
+            'gemini': 'https://gemini.com/',
+            'gdax': 'https://pro.coinbase.com/',
+              'kraken': 'https://www.kraken.com/en-us',
+              'bitstamp': 'https://www.bitstamp.net/',
+              'bitfinex': 'https://www.bitfinex.com',
+              'cex': 'https://cex.io/',
+              'poloniex': 'https://poloniex.com/',
+              'binance': 'https://www.binance.com/',
+              'bittrex': 'https://bittrex.com/',
+              'hitbtc': 'https://hitbtc.com/',
+              'bitbay': 'https://bitbay.net/en',
+              'okcoin': 'https://www.okcoin.com/',
+              'okex': 'https://www.okex.com/'
+          },
+
+          crypto_links: {
+            'btc': 'https://bitcoin.org/en/',
+              'trx': 'https://tron.network/',
+              'xmr': 'https://www.getmonero.org/',
+              'bch': 'https://www.bitcoincash.org/',
+              'ltc': 'https://litecoin.org/',
+              'eos': 'https://eos.io/',
+              'eth': 'https://ethereum.org/',
+              'xrp': 'https://ripple.com/xrp/',
+              'ada': 'https://www.cardano.org/',
+              'xlm': 'https://www.stellar.org/',
+              'xtz': 'https://tezos.com/',
+              'neo': 'https://neo.org/',
+              'dash': 'https://www.dash.org/',
+              'etc': 'https://ethereumclassic.org/',
+              'zec': 'https://z.cash/',
+              'xem': 'https://nem.io/',
+              'doge': 'https://dogecoin.com/',
+              'qtum': 'https://qtum.org/en',
+              'btg': 'https://bitcoingold.org/'
+
+
+          }
       };
     },
     methods: {
