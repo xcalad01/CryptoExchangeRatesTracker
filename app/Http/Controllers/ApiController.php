7a47e354586c5704b2c7ef62e70a2767cfbecc6d
Filip Caladi <filip.caladi@kiwi.com>
2020-05-12 21:21:35 +0200

    trying some fix

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1185,7 +1185,7 @@ class ApiController extends Controller
             ),
             main_table AS (
 	            SELECT
-		        SUM(\"ch\".\"Volume\" * ((\"Open\" + \"High\" + \"Low\") / 3)) AS \"Volume_Exchange\",
+		        SUM(\"ch\".\"Volume\" * \"Close\") AS \"Volume_Exchange\",
 		        AVG((\"ch\".\"Open\" + \"ch\".\"Close\" + \"ch\".\"High\") / 3 / \"fh1\".\"Value_USD\" * \"fh2\".\"Value_USD\") AS \"Price\",
 		        to_timestamp(floor((extract('epoch' FROM to_timestamp(\"ch\".\"Timestamp\")) / {$range})) * {$range} + \"ov\".offset_val) AT TIME ZONE 'UTC' AS \"interval_alias\"
 	        FROM
@@ -1217,7 +1217,7 @@ class ApiController extends Controller
                 \"interval_alias\"
         )
         SELECT
-            SUM(\"Volume_Exchange\" / \"Volume_Sum\" * 8900),
+            SUM(\"Volume_Exchange\" / \"Volume_Sum\" * \"Price\"),
             \"st\".\"interval_alias\"
         FROM
             main_table AS \"mt\",
