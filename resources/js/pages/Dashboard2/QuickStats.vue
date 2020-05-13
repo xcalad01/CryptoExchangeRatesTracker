972ea9b9abc68df00f338fd7bba445f5805c85f2
Filip Caladi <filip.caladi@kiwi.com>
2020-05-13 19:47:38 +0200

    fix

--- resources/js/pages/Dashboard2/QuickStats.vue
+++ resources/js/pages/Dashboard2/QuickStats.vue
@@ -19,7 +19,7 @@
 
             <div class="p-col-12 p-lg-4">
                 <div class="card">
-                    <span class="title"> Circulatting <br /> / Total Supply </span>
+                    <span class="title"> Circulatting / <br />  Total Supply </span>
                     <span class="detail"> Publicly available and circulating in the market </span>
                     <span class="span_value"> {{circulating_supply}} / <br /> {{total_supply}} </span>
                 </div>
