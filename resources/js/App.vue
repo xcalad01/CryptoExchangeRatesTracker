72fe7cc253960dcd5e3cb348729b4ff618e54c6e
Filip Caladi <filip.caladi@kiwi.com>
2020-05-27 11:35:37 +0200

    Last updates

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,28 +1,19 @@
+<!--
+ * App.vue
+ *
+ * Main component
+ * @author     Filip Caladi (xcalad01)
+ * @license    MIT
+ -->
+
 <template>
     <div>
-        <notifications></notifications>
         <router-view :key="$route.fullPath"></router-view>
     </div>
 </template>
 
 <script>
-    export default {
-        methods: {
-            disableRTL() {
-                if (!this.$rtl.isRTL) {
-                    this.$rtl.disableRTL();
-                }
-            },
-            toggleNavOpen() {
-                let root = document.getElementsByTagName('html')[0];
-                root.classList.toggle('nav-open');
-            }
-        },
-        mounted() {
-            this.$watch('$route', this.disableRTL, { immediate: true });
-            this.$watch('$sidebar.showSidebar', this.toggleNavOpen)
-        }
-    };
+    export default {};
 </script>
 
 <style lang="scss"></style>
