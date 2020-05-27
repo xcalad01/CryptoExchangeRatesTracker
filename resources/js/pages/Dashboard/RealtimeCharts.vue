3f1bfd85a39acfd2e1e72aa9f969992febe99f3d
Filip Caladi <filip.caladi@kiwi.com>
2020-05-27 16:23:10 +0200

    Last updates

--- resources/js/pages/Dashboard/RealtimeCharts.vue
+++ resources/js/pages/Dashboard/RealtimeCharts.vue
@@ -260,7 +260,7 @@
         }
         else{
           this.real_time_data.push([date * 1000, data['data'][0][1]]);
-          this.realtime_chart.series[0].addPoint(this.real_time_data[this.real_time_data.length - 1]);
+          this.realtime_chart.series[0].addPoint(this.real_time_data[this.real_time_data.length - 1], true, true);
           if (data['data']){
             this.last_realtime_value = Humanize.formatNumber(data['data'][0][1], 3);
           }
